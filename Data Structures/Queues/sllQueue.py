047582fe433c3b732369562d90e88cef6cf9ef7f
alztr2908 <alztr2908@gmail.com>
2023-07-14 17:54:48 +0800

    init sllQueue.py singly linked list implementation of queue

---
+++ Data Structures/Queues/sllQueue.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class sllQueue:
+    def __init__(self,head=None):
+        self.head = head
+        self.size = 0
+    
+    """
+    Helper Methods
+    """
+    def __isEmpty(self):
+        pass
+    
+    def isEmpty(self):
+        pass
+    
+    def peeking(self):
+        pass
+    
+    def contains(self):
+        pass
+    
+    def indexOf(self):
+        pass
+
+    """
+    Enqueue
+    """
+    def enqueue(self):
+        pass
+
+    """
+    Dequeue
+    """
+    def dequeue(self):
+        pass
+    
+    def removal(self):
+        pass
+
+    """
+    Print
+    """
+    def print(self):
+        pass
