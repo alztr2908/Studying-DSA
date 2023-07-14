97f0e90efb4096bf8ab22c03075fbe0ad3027030
alztr2908 <alztr2908@gmail.com>
2023-07-14 18:07:47 +0800

    init sllQueue.py singly linked list implementation of queue

--- Data Structures/Queues/sllQueue.py
+++ Data Structures/Queues/sllQueue.py
@@ -6,19 +6,26 @@ class Node:
 class sllQueue:
     def __init__(self,head=None):
         self.head = head
+        self.tail = None
         self.size = 0
     
     """
     Helper Methods
     """
     def __isEmpty(self):
-        pass
+        if self.size == 0:
+            return True
+        
+        return False
     
     def isEmpty(self):
-        pass
+        if self.__isEmpty():
+            return print("Empty queue")
+        
+        return print("NOT Empty queue: {self.size}")
     
-    def peeking(self):
-        pass
+    def peek(self):
+        return print(f'Current HEAD: {self.head.value}')
     
     def contains(self):
         pass
@@ -29,8 +36,16 @@ class sllQueue:
     """
     Enqueue
     """
-    def enqueue(self):
-        pass
+    def enqueue(self,new_node):
+        if self.__isEmpty():
+            self.head = self.tail = new_node
+            self.size += 1
+            return 
+        
+        self.tail.next = new_node
+        self.tail = new_node
+        self.size += 1
+
 
     """
     Dequeue
@@ -45,4 +60,26 @@ class sllQueue:
     Print
     """
     def print(self):
-        pass
+        current = self.head
+
+        while current:
+            print(f'{current.value}',end="->")
+            current = current.next
+        print("None")
+        print()
+
+node = sllQueue()
+
+e1 = Node(1)
+e2 = Node(2)
+e3 = Node(3)
+e4 = Node(4)
+e5 = Node(5)
+
+node.enqueue(e1)
+node.enqueue(e2)
+node.enqueue(e3)
+node.enqueue(e4)
+
+node.isEmpty()
+node.print()
