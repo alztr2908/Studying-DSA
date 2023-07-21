c1ae4cef4e140c4c462f5a6c1fd6257f1a7532d5
alztr2908 <alztr2908@gmail.com>
2023-07-21 15:47:25 +0800

    added insertionSort for sorting algo dir

--- Algorithms/Sorting/insertionSort.py
+++ Algorithms/Sorting/insertionSort.py
@@ -3,13 +3,15 @@ def insertionSort(nums):
         j = i
 
         while nums[j] > nums[j+1] and j >= 0:
+            # Swap
             temp = nums[j]
             nums[j] = nums[j+1]
             nums[j+1] = temp
+
             j -= 1
 
     print(nums)
 
 
-nums = [64, 34, 25, 12, 22, 11, 90]
+nums = [64, 34, 25, 12, 22, 11, -90]
 insertionSort(nums)
