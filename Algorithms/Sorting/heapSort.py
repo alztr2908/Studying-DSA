b5db5dad41749f33623c2664fa4ce7cf0e8e703d
alztr2908 <alztr2908@gmail.com>
2023-07-26 04:04:26 +0800

    finish heapSort.py

--- Algorithms/Sorting/heapSort.py
+++ Algorithms/Sorting/heapSort.py
@@ -5,48 +5,42 @@
 """
 
 
+# Build a heap
 def buildMaxHeap(arr):
-    n = len(arr)-1
-    for i in range(n//2, -1, -1):
-        heapify(arr, i)
+    n = len(arr)
+    for i in range(n//2-1, -1, -1):
+        heapify(arr, n, i)
+
+# Maintaining heap property thru recursion
 
 
-def heapify(arr, p):
+def heapify(arr, N, p):
     left = 2*p+1
     right = 2*p+2
+    max = p
 
-    # print(left)
-    # print(right)
-    # # print(arr[p])
-    # print(len(arr)-1)
-    if left <= len(arr)-1 and arr[left] > arr[p]:
+    if left < N and arr[left] > arr[p]:
         max = left
-    else:
-        max = p
 
-    if right <= len(arr) - 1 and arr[right] > arr[max]:
+    if right < N and arr[right] > arr[max]:
         max = right
-
-    # print(max)
-    # print(p)
-    # print()
 
     if max != p:
         arr[max], arr[p] = arr[p], arr[max]
-        heapify(arr, max)
+        heapify(arr, N, max)
 
 
 def heapSort(arr):
+    N = len(arr)
     buildMaxHeap(arr)
-    print(arr)
-    n = len(arr)-1
-
-    while n >= 0:
-        arr[n], arr[0] = arr[0], arr[n]
-        n -= 1
-        heapify(arr[:n], 0)
+    for i in range(N-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
     print(arr)
 
 
 arr = [1, 7, 6, 4, 3, 2, 5]
+arr2 = [12, 11, 13, 5, 6, 7]
 heapSort(arr)
+print()
+heapSort(arr2)
