ce78b768c2b5045cbfa7feb9c08590ead1af03a0
alztr2908 <alztr2908@gmail.com>
2023-07-31 05:05:51 +0800

    add 3sum leetcode q solution by implementing case 3

--- Leetcode Questions/15. 3Sum/threeSum.py
+++ Leetcode Questions/15. 3Sum/threeSum.py
@@ -10,11 +10,13 @@ if abs(res) == abs(curr) and (curr != p1 or curr != p2):
 case 1: same magnitude but different direction + zero = 0
     [1,-1,0] = 0
     [-2,2,0] = 0
-case 2: same magnitude and same direction + same magnitude of two num but different direction = 0 
+case 2: same magnitude and same direction + same magnitude of two num but different direction = 0
     [1,1,-2] = 0
     [-1,-1,2] = 0
-
-
+case 3: two elements equate to -3 + 3
+    [-5,2,3]
+case 4 two elements equate to 3 + -3
+    [5,-2,-3]
 """
 
 
@@ -25,6 +27,7 @@ def threeSum(nums):
     # Handles zero edge-case since i can't think of any solution
     numFreq = {0: 0}
     for num in nums:
+        print(num)
         if num == 0:
             numFreq[num] += 1
         if num in twoSum:
@@ -33,6 +36,11 @@ def threeSum(nums):
             twoSum[-num].append(num)
         else:
             twoSum[num] = []
+
+        if -(num + 3) in twoSum:
+            twoSum[-(num + 3)].append(num)
+        elif -(num - 3) in twoSum:
+            twoSum[-(num - 3)].append(num)
     print(twoSum)
     ans_container = []
     # If value is not -inf, then traverse to nums to find 0 or same magnitude
@@ -43,7 +51,7 @@ def threeSum(nums):
             for num in nums:
                 if key == 0 and val == 0 and numFreq[0] < 3:
                     continue
-                if key+val == -num:
+                if key+val+num == 0:
                     ans = [key, val, num]
                     # allows no duplication of elements
                     if ans not in ans_container:
@@ -54,4 +62,8 @@ def threeSum(nums):
 
 nums = [-1, 0, 1, 2, -1, -4]
 nums = [-1, 0, 1]
+nums = [5, -2, -3]
+nums = [5, -2, -3, 7, -4]
+nums = [3, 0, -2, -1, 1, 2]
+
 print(threeSum(nums))
