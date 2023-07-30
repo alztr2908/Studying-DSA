eef116d867f16e598b3d434790bab34fc03f3b95
alztr2908 <alztr2908@gmail.com>
2023-07-31 03:43:39 +0800

    add 3sum leetcode q that uses hashmap implementation covering case 1 and case 2 in problem constraints. O(k*n*n) solution where k = to len(array_value) in dictionary value

---
+++ Leetcode Questions/15. 3Sum/threeSum.py
@@ -0,0 +1,57 @@
+"""
+[-1,0,1,2,-1,-4]
+  |        |  |
+p1 = -1
+p2 = -4
+res = curr + temp
+if abs(res) == abs(curr) and (curr != p1 or curr != p2):
+    append(curr,p1,p2)
+
+case 1: same magnitude but different direction + zero = 0
+    [1,-1,0] = 0
+    [-2,2,0] = 0
+case 2: same magnitude and same direction + same magnitude of two num but different direction = 0 
+    [1,1,-2] = 0
+    [-1,-1,2] = 0
+
+
+"""
+
+
+def threeSum(nums):
+    # store element with same magnitude either same direction or not.
+    # if there is no same magnitude, equate it to -inf
+    twoSum = dict()
+    # Handles zero edge-case since i can't think of any solution
+    numFreq = {0: 0}
+    for num in nums:
+        if num == 0:
+            numFreq[num] += 1
+        if num in twoSum:
+            twoSum[num].append(num)
+        elif -num in twoSum:
+            twoSum[-num].append(num)
+        else:
+            twoSum[num] = []
+    print(twoSum)
+    ans_container = []
+    # If value is not -inf, then traverse to nums to find 0 or same magnitude
+    for key, value in twoSum.items():
+        if len(value) == 0:
+            continue
+        for val in value:
+            for num in nums:
+                if key == 0 and val == 0 and numFreq[0] < 3:
+                    continue
+                if key+val == -num:
+                    ans = [key, val, num]
+                    # allows no duplication of elements
+                    if ans not in ans_container:
+                        ans_container.append(ans)
+
+    return ans_container
+
+
+nums = [-1, 0, 1, 2, -1, -4]
+nums = [-1, 0, 1]
+print(threeSum(nums))
