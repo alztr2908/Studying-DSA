6d36bc89e5ffdf0b70a481960a6e9ad4a61b05f7
alztr2908 <alztr2908@gmail.com>
2023-08-02 00:48:17 +0800

    finish 3sum that uses sliding window method in leetcode dir

--- Leetcode Questions/15. 3Sum/threeSum.py
+++ Leetcode Questions/15. 3Sum/threeSum.py
@@ -20,7 +20,7 @@ case 4 two elements equate to 3 + -3
 """
 
 
-def threeSum(nums):
+def threeSum_meSolution(nums):
     # store element with same magnitude either same direction or not.
     # if there is no same magnitude, equate it to -inf
     twoSum = dict()
@@ -64,13 +64,66 @@ nums = [5, -2, -3]
 nums = [5, -2, -3, 7, -4]
 nums = [3, 0, -2, -1, 1, 2]
 nums = [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]
-print(nums)
+# print(nums)
+# print(threeSum_meSolution(nums))
+
+
+"""
+Solution 2: Neetcode solution
+
+example:
+[-1,0,1,2,-1,-4]
+
+Sort the array:
+[-4,-1,-1,0,1,2]
+
+Loop thru negatives assign a left and right pointer
+that slides with the following condition
+if less than 0: left++
+if greater than 0: right--
+
+if at the start of array the sum is already greater than zero, then early return
+
+"""
+
+
+def threeSum(nums: list[int]) -> list[int]:
+    nums.sort()
+    # print(nums)
+
+    three_container = []
+    for i in range(len(nums)-2):
+        # to avoid repition on previous elements
+        if i > 0 and nums[i-1] == nums[i]:
+            continue
+
+        left = i+1
+        right = len(nums)-1
+        while left < right:
+            res = [nums[i], nums[left], nums[right]]
+            # print(i, res)
+
+            if sum(res) < 0:
+                left += 1
+            elif sum(res) > 0:
+                right -= 1
+            else:
+                # if res not in three_container:
+                three_container.append(res)
+                while left < right and nums[left] == nums[left+1]:
+                    left += 1
+                left += 1
+                while left < right and nums[right] == nums[right-1]:
+                    right -= 1
+                right -= 1
+
+    return three_container
+
+
+nums = [-1, 0, 1]
+nums = [5, -2, -3]
+nums = [5, -2, -3, 7, -4]
+nums = [3, 0, -2, -1, 1, 2]
+nums = [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]
+nums = [-1, 0, 1, 2, -1, -4]
 print(threeSum(nums))
-
-
-"""
-[-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]
-
--1: [1]
-0: []
-"""
