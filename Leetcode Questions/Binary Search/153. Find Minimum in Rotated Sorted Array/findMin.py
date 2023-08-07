6f3dd5b402d94242908818596223a56b0991e03a
alztr2908 <alztr2908@gmail.com>
2023-08-07 22:15:06 +0800

    finish findMin in a rotated sorted array that uses binary search implementation

---
+++ Leetcode Questions/Binary Search/153. Find Minimum in Rotated Sorted Array/findMin.py
@@ -0,0 +1,36 @@
+def findMin(nums) -> int:
+    l, r = 0, len(nums)-1
+
+    while l < r:
+        m = (r-l)//2+l
+        print(nums[l:r+1])
+        if nums[m] > nums[r]:
+            l = m+1  # right side
+        else:
+            r = m  # left side
+
+    return nums[l]
+
+
+nums = [3, 4, 5, 1, 2]
+print(findMin(nums))
+nums = [6, 7, 0, 1, 2, 3, 4, 5]
+print(findMin(nums))
+nums = [1, 2, 3, 4, 5]
+print(findMin(nums))
+
+"""
+[3, 4, 5, 1, 2]
+ L     M     R
+ [1,2]
+ [1]
+
+[6,7,0,1,2,3,4,5]
+ L     M       R
+ [6,7,0]
+ [0]  
+if nums[mid] >= nums[left]
+    search right side
+else:
+    search left side
+"""
